cf30c1a5d3949798a3cd5c1caaef63eb27b58b43
Ilqar Ilyasov <imilqar@gmail.com>
2018-09-10 16:54:31 -0500

    AskQuestionViewController. submitQuestion bar button function completed.

--- q-and-a/View Controller/AskQuestionViewController.swift
+++ q-and-a/View Controller/AskQuestionViewController.swift
@@ -12,31 +12,20 @@ class AskQuestionViewController: UIViewController {
 
     @IBOutlet weak var askerNameTextField: UITextField!
     @IBOutlet weak var questionTextView: UITextView!
+    var questionController: QuestionController?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     @IBAction func submitQuestionBarButtonAction(_ sender: Any) {
+        guard let question = questionTextView.text,
+              let asker = askerNameTextField.text else { return }
         
+        if question == "" && asker == "" {
+            questionController?.createQuestion(question: question, asker: asker)
+        }
+        
+        navigationController?.popToRootViewController(animated: true)
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
